5b1c8e1d19052f25fc658ef7769a2cc0591c0911
Jakob Persons <jakobep24@gmail.com>
2023-06-07 10:40:39 -0400

    configure apollo driver, check first query

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PetsModule } from './pets/pets.module';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'schema.gql'
+    }),
+    PetsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
